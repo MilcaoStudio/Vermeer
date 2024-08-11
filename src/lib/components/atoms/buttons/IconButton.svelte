66e380357f14556d194ba42568ad6623909081d5
MrChileball <chileball5@gmail.com>
2024-08-11 00:00:46 -0400

    IconButton component

---
+++ src/lib/components/atoms/buttons/IconButton.svelte
@@ -0,0 +1,72 @@
+<script>
+
+    export let hasBackground = false;
+    export let isSmall = false;
+
+    $: mode = hasBackground ? 'background' : 'transparent';
+    $: size = isSmall ? 'tiny' : 'regular';
+
+
+</script>
+
+<div class={[mode, size].join(' ')}>
+    <slot>
+       
+    </slot>
+</div>
+
+
+<style>
+    div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: var(--borderRadius--iner);
+        transition: all 0.3s ease 0s;
+
+    }
+
+    .background{
+        background: var(--bgMain);
+    }
+    .background:hover{
+        background: var(--bgHover);
+    }
+    div:hover :global( svg ){
+        color: var(--textMain);
+    }
+    .background:active{
+        background: var(--colorPrimary);
+    }
+    div:active :global( svg ){
+        color: var(--textMain);
+    }
+    :global(svg){
+        height: 24px;
+        width: 24px;
+        color: var(--textDim);
+    }
+
+    .transparent{
+        background: transparent;
+    }
+    .transparent:active :global(svg){
+        color: var(--colorPrimary);
+    }
+
+    /* SIZES */
+
+    .regular{
+        width: 32px;
+        height: 32px;
+    }
+    .tiny{
+        width: 22px;
+        height: 22px;
+    }
+    .tiny :global(svg){
+        width: 22px;
+        height: 22px;
+    }
+
+</style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 // Reexport your entry components here
 import "./styles.css";
+
+
 export { default as H1 } from "./components/atoms/text/H1.svelte";
 export { default as H2 } from "./components/atoms/text/H2.svelte";
 export { default as H3 } from "./components/atoms/text/H3.svelte";
@@ -7,3 +9,6 @@ export { default as P1 } from "./components/atoms/text/P1.svelte";
 export { default as P2 } from "./components/atoms/text/P2.svelte";
 export { default as P3 } from "./components/atoms/text/P3.svelte";
 export { default as P4 } from "./components/atoms/text/P4.svelte";
+
+
+export { default as IconButton } from "./components/atoms/buttons/IconButton.svelte";
