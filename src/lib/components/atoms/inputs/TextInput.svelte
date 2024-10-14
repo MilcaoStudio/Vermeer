c4781bd964dec0e6204ca2d77d99bf626f971259
MrChileball <chileball5@gmail.com>
2024-10-14 10:52:32 -0300

    TextInput component

---
+++ src/lib/components/atoms/inputs/TextInput.svelte
@@ -0,0 +1,28 @@
+<script>
+    let type = "text";
+</script>
+
+<input {type} />
+
+<style>
+    p {
+    font-size: 14px;
+    font-weight: 400;
+    width: 328px;
+    margin: 0;
+    margin-top: 17px;
+  }
+
+  
+  input {
+    font-size: 16px;
+    background-color: var(--bgHighlight);
+    color: var(--textMain);
+    margin: 0;
+    margin-top: 16px;
+    border: none;
+    height: 32px;
+    border-radius: 8px;
+    padding: 0;
+  }
+</style>
--- src/stories/components/modal/Modal.stories.svelte
+++ src/stories/components/modal/Modal.stories.svelte
@@ -1,6 +1,7 @@
 <script context="module">
-import { Modal,} from "$lib/index.js";
+import { Modal , TextInput } from "$lib/index.js";
   import { Story, Template } from "@storybook/addon-svelte-csf";
+
 
   export const meta = {
     title: "Modals/Modal",
@@ -13,7 +14,9 @@ import { Modal,} from "$lib/index.js";
     <span slot="title">{args.title}</span>
     <p>{args.description}</p>
     <p class="serverName">Server name</p>
-    <input type="text" />
+    <TextInput >
+
+    </TextInput>
   </Modal>
 </Template>
 
@@ -21,19 +24,27 @@ import { Modal,} from "$lib/index.js";
   <Modal open />
 </Story>
 
+<Story name="Testing" >
+  
+  <Modal open>
+    <span slot="title">Coquito</span>
+    <p class="serverName">Server name</p>
+    <div slot="content">
+      <TextInput >
+
+      </TextInput>
+      <TextInput >
+  
+      </TextInput>
+    </div>
+    
+  </Modal>
+</Story>
+
 <Story name="Create Server" args={{open: true, title: "Create a server", description: "When you create a server, you are accepting our terms of server and guidelines for moderation"}} />
 
 
-
 <style>
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    width: 328px;
-    margin: 0;
-    margin-top: 17px;
-  }
-
   .serverName {
     font-size: 16px;
     color: var(--textMain);
@@ -42,15 +53,4 @@ import { Modal,} from "$lib/index.js";
     font-weight: 400;
   }
 
-  input {
-    font-size: 16px;
-    background-color: var(--bgHighlight);
-    color: var(--textMain);
-    margin: 0;
-    margin-top: 16px;
-    border: none;
-    height: 32px;
-    border-radius: 8px;
-    padding: 0;
-  }
 </style>
